b8c54b2323c79713a50f444e621aaa2d3f8ced80
HoYoungKang <windwalk11@nate.com>
2016-06-23 14:26:41 +0900

    함수 정리, 객체화

--- DS프로젝트.py
+++ DS프로젝트.py
@@ -1,28 +1,20 @@
 import re
 
-print(55 * '-')
-print("                      M E N U                      ")
-print(55 * '-')
-print("0. Read Data Files")
-print("1. Display Statistics ")
-print("2. Top 5 Most Tweeted Words")
-print("3. Top 5 Most Tweeted Users")
-print("4. Find Users who tweeted a word")
-print("5. Find All People who are friends of the above users")
-print("6. Delete Users who mentioned a word ")
-print("7. Delete All Users who mentioned a word ")
-print("8. Find Strongly Connected Components")
-print("9. Find Shortest Path from a given user")
-print("99. Quit")
-print(55 * '-')
+m = dict()
 
-# Get input
-choice = input('Select Menu : ')
+def sum():
+    res = 0
+    for i in m:
+        res += m[i]
+    return res
 
-# Convert string to int type #
-choice = int(choice)
+def count(word):
+    if m.get(word):
+        m[word] += 1
+    else: m[word] = 1
 
 def read_file():
+    print("Reading Data Files...")
     ucount = 0
     u = open('user.txt')
     for line in u:
@@ -45,21 +37,63 @@ def read_file():
        #print(line)
     print('Total Tweets: ', wcount/4)
 
-m = dict()
+def statistic():
+    print("Displaying Statistics...")
+    f = open('friend.txt')
+    for line in f:
+        line = line[0:-1]
+        count(line)
+    del m['']
+    maximumf = max(m, key=m.get)
+    minimumf = min(m, key=m.get)
+    print("Average Number of Friends:", sum() / len(m))
+    print("Maximum Number of Friends:", maximumf, "=>", m[maximumf])
+    print("Minimum Number of Friends:", minimumf, "=>", m[minimumf])
+    m.clear()
 
-def sum():
-    res = 0
-    for i in m:
-        res += m[i]
-    return res
+    for i, l in enumerate(open('word.txt')):
+        if i % 4 == 0:
+            count(l)
+    maximumt = max(m, key=m.get)
+    minimumt = min(m, key=m.get)
+    print("Average Tweet per User:", sum() / len(m))
+    print("Maximum Tweet per User:", maximumt.rstrip(), "=>", m[maximumt])
+    print("Minimum Tweet per User:", minimumt.rstrip(), "=>", m[minimumt])
+    m.clear()
 
-def count(word):
-    if m.get(word):
-        m[word] += 1
-    else: m[word] = 1
+def mostword():
+    print("Showing Top 5 Most Tweeted Words...")
+    for i, l in enumerate(open('word.txt')):
+        if i % 4 == 2:
+            count(l)
+    top = sorted(m, key=m.get, reverse=True)[:5]
+    for i in range(5):
+        print(top[i].rstrip(), ":", m[top[i]])
 
+def mostuser():
+    print("Showing Top 5 Most Tweeted Users...")  #Tweet 많이 한 사람
+    for i, l in enumerate(open('word.txt')):
+        if i % 4 == 0:
+            count(l)
+    top = sorted(m, key=m.get, reverse=True)[:5]
+    for i in range(5):
+        print(top[i].rstrip(), ":", m[top[i]])
+
+def userfind():
+    print("Finding Users who tweeted a word...")
+    text = str(input("Word: "))
+    total = 0
+    lst = []
+    for l in open('word.txt'):
+        lst.append(l)
+        total += 1
+        if text == l[:-1]:
+            print(lst[total - 3].rstrip(), "=>", l)
+    return lst
 # Take action as per selected menu-option
-'''   Top 5 friends
+
+def userfriend():
+    users = userfind()
     f = open('friend.txt')
     for line in f:
         line = line[0:-1]
@@ -69,86 +103,54 @@ def count(word):
     for i in range(5):
         print(top[i],m[top[i]])
 '''
-if choice == 0:
-    print("Reading Data Files...")
-    read_file()
-elif choice == 1:
-    print("Displaying Statistics...")
-    f = open('friend.txt')
-    for line in f:
-        line = line[0:-1]
-        count(line)
-    del m['']
-    maximumf = max(m, key = m.get)
-    minimumf = min(m, key = m.get)
-    print("Average Number of Friends:", sum()/len(m))
-    print("Maximum Number of Friends:", maximumf, "=>", m[maximumf])
-    print("Minimum Number of Friends:", minimumf, "=>", m[minimumf])
-    m.clear()
+if __name__ == '__main__':
+    class Choice():
+        def __init__(self, desc, fn):
+            self.desc , self.fn =  desc, fn
 
-    for i,l in enumerate(open('word.txt')):
-        if i%4 == 0:
-            count(l)
-    maximumt = max(m, key = m.get)
-    minimumt = min(m, key = m.get)
-    print("Average Tweet per User:", sum()/len(m))
-    print("Maximum Tweet per User:", maximumt.rstrip(), "=>", m[maximumt])
-    print("Minimum Tweet per User:", minimumt.rstrip(), "=>", m[minimumt])
-    m.clear()
-elif choice == 2:
-    print("Showing Top 5 Most Tweeted Words...")
-    for i, l in enumerate(open('word.txt')):
-        if i % 4 == 2:
-            count(l)
-    top = sorted(m, key=m.get, reverse=True)[:5]
-    for i in range(5):
-        print(top[i].rstrip(), ":", m[top[i]])
-elif choice == 3:
-    print("Showing Top 5 Most Tweeted Users...")  #Tweet 많이 한 사람
-    for i, l in enumerate(open('word.txt')):
-        if i % 4 == 0:
-            count(l)
-    top = sorted(m, key=m.get, reverse=True)[:5]
-    for i in range(5):
-        print(top[i].rstrip(), ":", m[top[i]])
-elif choice == 4:
-    print("Finding Users who tweeted a word...")
-    text = str(input("Word: "))
-    total = 0
-    lst = []
-    for l in open('word.txt'):
-        lst.append(l)
-        total += 1
-        if text == l[:-1]:
-            print(lst[total-3].rstrip(), "=>", l)
-elif choice == 5:
-    print("Find All People who are friends of the above users...")
-    text = str(input("Word: "))
-    totalw = 0
-    totalf = 0
-    lstw = []
-    lstf = []
-    for l in open('word.txt'):
-        lstw.append(l)
-        totalw += 1
-        if text == l[:-1]:
-            print("User who tweet a word: ", lstw[totalw - 3].rstrip())
-            break
-    for ll in open('friend.txt').read().split():
-        lstf.append(ll)
-        totalf += 1
-    print(lstf)
-        #if lstw[totalw - 3].rstrip() == ll[:-1]:
-            #print("All Users who are Friend: ", lstf[totalf])
-elif choice == 6:
-    print("Deleting Users...")
-elif choice == 7:
-    print("Deleting All Users...")
-elif choice == 8:
-    print("Finding SCC...")
-elif choice == 9:
-    print("Finding Shortest Path...")
-elif choice == 99:
-    print("Good Bye")
-else:  # default
-    print("Invalid number. Try again...")
+    choices = list(map(Choice,('Read Data Files', 'Display Statistics', 'Top 5 Most Tweeted Words', 'Top 5 Most Tweeted Users','Find Users who tweeted a word', 'Find All People who are friends of the above users','Delete Users who mentioned a word','Delete All Users who mentioned a word','Find Strongly Connected Components','Find Shortest Path from a given user'),
+                       (read_file, statistic, mostword, mostuser, userfind, None, None, None, None)))
+    while True:
+        print(55 * '-')
+        print("                      M E N U                      ")
+        print(55 * '-')
+        for i, c in enumerate(choices):
+            print(i, ":", c.desc)
+        print("99. Quit")
+        print(55 * '-')
+
+        # Get input
+        choice = input('Select Menu : ')
+        # Convert string to int type #
+
+        if choice == '99':
+            print("Good Bye")
+            exit()
+        elif not choice.isdecimal() or int(choice) > len(choices):  # default
+            print("Invalid input. Try again...")
+        else:
+            choice = int(choice)
+            choices[choice].fn()
+'''
+    elif choice == 5:
+        print("Find All People who are friends of the above users...")
+        userfind()
+
+        text = str(input("Word: "))
+        totalw = 0
+        totalf = 0
+        lstw = []
+        lstf = []
+        for l in open('word.txt'):
+            lstw.append(l)
+            totalw += 1
+            if text == l[:-1]:
+                print("User who tweet a word: ", lstw[totalw - 3].rstrip())
+                break
+        for ll in open('friend.txt').read().split():
+            lstf.append(ll)
+            totalf += 1
+        print(lstf)
+            #if lstw[totalw - 3].rstrip() == ll[:-1]:
+                #print("All Users who are Friend: ", lstf[totalf])
+'''
